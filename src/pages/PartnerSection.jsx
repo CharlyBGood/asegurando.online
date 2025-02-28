11cf402e7c7e38b01f6af72fa92be08cbb223341
charlybgood__x <bonavita.carlos@gmail.com>
2025-02-27 21:04:44 -0300

    add partners logos and edit css classes

--- src/pages/PartnerSection.jsx
+++ src/pages/PartnerSection.jsx
@@ -1,30 +1,23 @@
 import '../stylesheets/PartnerSection.css';
 
-// Import partner logos
-// Note: These would be replaced with your actual insurance partner logos
-import placeholder from '../assets/logos/atmLogo.png';
+import PartnersLogos from './utilities/PartnersLogos'
 
 const PartnerSection = () => {
-  // Partner data could come from a CMS or config file in a real implementation
-  const partners = [
-    { id: 1, name: 'Insurance Partner 1', logo: placeholder },
-    { id: 2, name: 'Insurance Partner 2', logo: placeholder },
-    { id: 3, name: 'Insurance Partner 3', logo: placeholder },
-    { id: 4, name: 'Insurance Partner 4', logo: placeholder },
-    { id: 5, name: 'Insurance Partner 5', logo: placeholder },
-  ];
-
-  return (
-    <section className="partner-section p-4">
-      <div className="container">
-        <h2>Nuestros socios:</h2>
-        <div className="flex flex-wrap">
-          {partners.map(partner => (
-            <img key={partner.id} src={partner.logo} alt={partner.name} />
+  
+  return (    
+      <div className="flex flex-col items-center p-4">
+        <h2 className='text-xl mb-4 text-center'>Compañías con las que operamos:</h2>
+        <div className="w-7/10 flex flex-wrap gap-4 p-4 justify-center">
+          {PartnersLogos.map(partner => (
+            <img 
+              className='w-20 h-20 object-contain rounded-lg shadow-md' 
+              key={partner.id} 
+              src={partner.logo} 
+              alt={partner.name} 
+            />
           ))}
         </div>
       </div>
-    </section>
   );
 };
 
---
+++ src/pages/utilities/PartnersLogos.js
@@ -0,0 +1,19 @@
+import atm from "../../assets/partners/atmLogo.png"
+import galicia from "../../assets/partners/galiciaseguros.png";
+import mapfre from "../../assets/partners/mapfre.png";
+import mercantilandina from "../../assets/partners/mercantilandinar.png";
+import provinciaseguros from "../../assets/partners/provinciaseguros.png";
+import libra from "../../assets/partners/libra.svg";
+import galeno from "../../assets/partners/galeno.svg";
+
+const PartnersLogos = [
+    { id: 1, name: 'ATM Seguros', logo: atm},
+    { id: 2, name: 'Galicia Seguros', logo: galicia },
+    { id: 3, name: 'Mapfre Seguros', logo: mapfre },
+    { id: 4, name: 'Mercantil Andina Seguros', logo: mercantilandina },
+    { id: 5, name: 'Provincia Seguros', logo: provinciaseguros },
+    { id: 6, name: 'Libra  Seguros', logo: libra },
+    { id: 7, name: 'Galeno Seguros', logo: galeno },
+  ];
+
+  export default PartnersLogos;
