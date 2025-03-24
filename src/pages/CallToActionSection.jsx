27d7a724fba0ea681ee72a5b3aa6929b0de9136a
charlybgood__x <bonavita.carlos@gmail.com>
2025-03-24 16:29:15 -0300

    create separate component for CTA btn / add new style

--- src/pages/CallToActionSection.jsx
+++ src/pages/CallToActionSection.jsx
@@ -1,23 +1,19 @@
 const atm = "https://res.cloudinary.com/dr8pwzxzn/image/upload/v1741819703/atmLogo_zgzx7l.png"
+import { CTAButton } from "./utilities/CTAButton";
 
 const CallToActionSection = () => {
   return (
     <div className="flex-1 p-4 text-blueGray flex flex-col justify-center items-start">
       <h1 className='mb-4 text-4xl'>Seguro, simple. Rápido.</h1>
       <p className='mb-2 text-xl'>
-        Cotizá tu vehículo y asegurá en minutos con nuestra herramienta en 
+        Cotizá tu vehículo y asegurá en minutos con nuestra herramienta en
         <img
-        className='ml-2 inline w-15 h-15 object-contain rounded-lg shadow-md'
-        src={atm}
-        alt="Logo of ATM Seguros"
-      />
+          className='ml-2 inline w-15 h-15 object-contain rounded-lg shadow-md'
+          src={atm}
+          alt="Logo of ATM Seguros"
+        />
       </p>
-      <button
-        className="rounded-full p-(--cta-btn-padding) bg-lightBlue text-lg cursor-pointer hover:bg-lightBlueHover text-Black">
-        <a href="https://ecommerce.atmseguros.com.ar/" target="_blank" rel="noopener noreferrer">
-          ¡COTIZA YA!
-        </a>
-      </button>
+      <CTAButton />
     </div>
   );
 };
---
+++ src/pages/utilities/CTAButton.jsx
@@ -0,0 +1,13 @@
+export const CTAButton = () => (
+  <div
+    className="button w-40 h-16 bg-lightBlue hover:bg-lightBlueHover cursor-pointer select-none
+    active:translate-y-2  active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
+    active:border-b-[0px]
+    transition-all duration-150 [box-shadow:0_10px_0_0_#1b6ff8,0_15px_0_0_#1b70f841]
+    rounded-full border-[1px] border-blue-400"
+  >
+    <a className="flex flex-col justify-center items-center h-full font-bold text-Black text-lg " href="https://ecommerce.atmseguros.com.ar/" target="_blank" rel="noopener noreferrer">
+      ¡COTIZA YA!
+    </a>
+  </div>
+)
