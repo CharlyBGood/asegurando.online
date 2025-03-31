3c729c7ed16da3bf48402718ddebeb9c6b2d85e7
charlybgood__x <bonavita.carlos@gmail.com>
2025-03-31 19:47:15 -0300

    dix css on Contact component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ export const Contact = ({ openCloseModal }) => (
   <div className="modal-container-4">
     <div className="modal-content-4">
       <div className="modal-4-header">
-        <h1>Cómo encontrarnos</h1>
+        <h1>Contactanos</h1>
         <span className="close-4" onClick={openCloseModal}>×</span>
       </div>
       <div className="modal-display-4">
@@ -18,34 +18,35 @@ export const Contact = ({ openCloseModal }) => (
             VER MAPA </CTAButton>
           <h3 className="citas">Las citas deben ser acordadas previamente.</h3>
         </div>
+        <img
+          className="logoGold"
+          src="https://res.cloudinary.com/dr8pwzxzn/image/upload/v1741819729/logoHD2_l7jags.png"
+          alt="Logo de Maschio y Asociados"
+        />
         <div className="modal-display-right">
           <h3 className="ubicacion">Contacto</h3>
+          <p>Contactanos por email a:</p>
           <h3>
             <a className="email" href="mailto:maschiomartin@gmail.com">
               maschiomartin@gmail.com
             </a>
           </h3>
-          <h3>+54 9 11 5699-9580</h3>
+          <h3>TEL.: +54 9 11 5699-9580</h3>
           <CTAButton href="tel:+54 9 11 5699-9580">
             <FaPhone />
           </CTAButton>
-          <img
-            className="logoGold"
-            src="https://res.cloudinary.com/dr8pwzxzn/image/upload/v1741819729/logoHD2_l7jags.png"
-            alt="Logo de Maschio y Asociados"
-          />
         </div>
       </div>
       <div className="footer-lightblue">
-          <div className="text-center text-lg">
-            <a
-              href="https://wa.me/5491156999580"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Contact us on WhatsApp"
-            >Hablemos por WhatsApp</a>
-          </div>
+        <div className="text-center text-lg">
+          <a
+            href="https://wa.me/5491156999580"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Contact us on WhatsApp"
+          >Hablemos por WhatsApp</a>
         </div>
+      </div>
     </div>
   </div>
 );
